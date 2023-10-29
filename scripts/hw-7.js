8a7d1f782d75bee87cf0f4acb0b8bf07f6c1a198
Роман Ласовский <roma_lasov@mail.ru>
2023-10-29 23:21:44 +0000

    1st part

---
+++ scripts/hw-7.js
@@ -0,0 +1,19 @@
+// Task 1
+let str = 'привет как дела';
+str = str.toUpperCase();
+console.log(str);
+
+// Task 2 функция возврата массива элементов начинающихся со строки
+const searchStart = (arr, str) => {
+    let result = arr.filter(item => item.toLowerCase().startsWith(str.toLowerCase()));
+    return result;
+  }
+
+// Task 3
+
+// Task 4
+// Task 5
+// Task 6
+// Task 7
+// Task 8
+// Task 9
