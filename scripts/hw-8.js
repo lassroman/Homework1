2a61920eac67f02bae0d37ddb9ae8c382f2a699d
Роман Ласовский <roma_lasov@mail.ru>
2023-11-02 18:40:03 +0000

    done

--- scripts/hw-8.js
+++ scripts/hw-8.js
@@ -40,3 +40,46 @@ console.log(filter([3, -4, 1, 9], isPositive));
     ];
     
 console.log(filter(people, isMale)); 
+
+// Task 3 вывести в консоль дату каждые 3 секудны на протяжении 30 секунд
+function printDate() {
+    console.log(new Date());
+}
+
+let count = 0;
+let intervalId = setInterval(() => {
+    printDate();
+    count += 3;
+    if (count >= 30) {
+        clearInterval(intervalId);
+        console.log("30 секунд прошло");
+    }
+}, 3000);
+
+//  Task 4 вывод ффункции через 1 сек
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);  
+}
+
+delayForSecond(function () {
+  console.log('Привет, Глеб!');
+})
+
+//  Task 5
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+			if(cb)
+            { cb(); }
+
+    }, 1000)
+}
+
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(function () {
+    sayHi('Глеб');
+});
